d62a7843428d7f1e052830e3928b7de76938df57
RylanFerrer <rylanferrer@gmail.com>
2019-07-11 10:57:39 -0700

    added a backdrop carousel

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
- const imgURL = "https://image.tmdb.org/t/p/w154";
+ const imgURL = "https://image.tmdb.org/t/p/";
 
 const settings = {
   async:true,
@@ -11,21 +11,56 @@ const settings = {
 }
 
 getnowPlaying();
+
 var $nowPlaying = $('#nowPlaying');
 
-
-//get
 function getnowPlaying() {
   settings.url = "/api/nowplaying";
   $.ajax(settings).done(function(response){
 
         $.each(response.results, function(i,item){
 
-            $nowPlaying.slick('slickAdd',"<div><img src='" + imgURL +  item.poster_path + "'/img>" + "<p>" + item.original_title +"</p>" + " </div>");
+            $nowPlaying.slick('slickAdd',"<div><img src='" + imgURL + "w154" + item.poster_path + "'/img>" + "<p>" + item.original_title +"</p>" + " </div>");
 
         });
     });
 
+}
+displayPopular();
+function displayPopular() {
+  settings.url = "/api/popular";
+
+  $.ajax(settings).done(function(response){
+    var i = 0;
+    while (i != 3)
+    {
+          random = getRandom(response.results.length);
+          if( i == 0)
+          {
+             img = imgURL + "original"+ response.results[random].backdrop_path;
+             console.log(img);
+             $("#populardisplay").append( "<div class='carousel-inner'>  <div class='carousel-item active '><div class = 'droptext'> <h2>" + response.results[random].original_title +  " </h2></div>  <img class='d-block w-10 img-fluid display-img' src=" +img +" alt='First slide'>  </div>");
+          }
+          else
+          {
+              img = imgURL + "original"+ response.results[random].backdrop_path;
+             $(".carousel-inner").append( " <div class='carousel-item display-img'> <div class = 'droptext'> <h2>" + response.results[random].original_title +  " </h2></div> <img class='d-block w-10 img-fluid' src=" +img +" alt='First slide'</div>");
+          }
+        i++;
+    }
+
+
+
+    });
+}
+
+
+
+//get random num function
+function getRandom(length)
+{
+    var random = Math.floor((Math.random() * length) + 1);
+    return random;
 }
 
 // settings for the carousel for movies
